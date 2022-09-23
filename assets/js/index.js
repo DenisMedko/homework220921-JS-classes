e706a9ab733acd459dd508797e815856afd3d124
DenisMedko <medko.denis76@gmail.com>
2022-09-23 11:07:36 +0300

    code refactored

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,27 +2,31 @@ class RangeValidator {
     #from;
     #to;
     #arr;
-    
     #fillArray() {
         if (this.#arr.length) {
             this.#arr.length = 0;
         }
         if (this.to > this.from) {
-            for (let i = this.from; i <= this.to - this.from + 1; i++) {
+            for (let i = this.from; i <= this.to + 1; i++) {
                 this.#arr.push(i);
             } 
         } else if(this.to === this.from) {
             this.#arr.push(this.from);
             this.#arr.push(this.to);
         } else {
-            throw new Error('Start value must be <= end value');
+            throw new Error(`Range array does'n created: start value ${this.from} must be <= than end value ${this.to}`);
         }
-
+    }
+    #checkTypeOfValue(obj) {
+        if (isNaN(obj.value) || !Number.isInteger(+obj.value)) {
+            throw new TypeError(`Value ${obj.name} ${obj.value} is not integer number`);    
+        }
+        return +obj.value;    
     }
     constructor(startValue, endValue){
-        this.#from = Math.min(+startValue, +endValue);
-        this.#to = Math.max(+startValue, +endValue);
-        this.#arr = [];
+        this.#from  = this.#checkTypeOfValue({name : 'from', value : startValue});
+        this.#to    = this.#checkTypeOfValue({name : 'to', value : endValue});
+        this.#arr   = [];
         this.#fillArray();
     }
     get from() {
@@ -31,20 +35,12 @@ class RangeValidator {
     get to() {
         return this.#to;
     }
-    set from(value) {
-        value = +value;
-        if (typeof value !== 'number' || !Number.isInteger(value)) {
-            throw new TypeError(`Value from ${value} is not integer number`);    
-        }
-        this.#from = value;
+    set from(val) {
+        this.#from = this.#checkTypeOfValue({name : 'from', value : val});
         this.#fillArray();
     }
-    set to(value) {
-        value = +value;
-        if (typeof value !== 'number' || !Number.isInteger(value)) {
-            throw new TypeError(`Value to ${value} is not integer number`);    
-        }
-        this.#to = value;
+    set to(val) {
+        this.#to = this.#checkTypeOfValue({name : 'to', value : val});
         this.#fillArray();
     }
     
@@ -53,34 +49,44 @@ class RangeValidator {
     }
 
     validate(val) {
-        val = +val;
-        if (typeof val !== 'number' || !Number.isInteger(val)) {
-            throw new TypeError(`Value ${val} is not integer number`);    
-        }
+        val = this.#checkTypeOfValue({name : 'your number', value : val});
         if (this.#arr.indexOf(val) < 0 ) {
-            throw new RangeError(`Number ${val} is out of range from ${rangeValidator.range[0]} to ${rangeValidator.range[this.range.length - 1]}`); 
+            throw new RangeError(`Your number ${val} is out of range from ${this.range[0]} to ${this.range[this.range.length - 1]}`); 
         }
         return val;
     }
 }
 
-const rangeValidator = new RangeValidator(1, '5');
-let validationResult;
 try {
-    validationResult = rangeValidator.validate('3'); 
+    const rangeValidator = new RangeValidator(1, '5');
+    let validationResult;
+    try {
+        validationResult = rangeValidator.validate('3'); 
+    } catch (err) {
+        validationResult = err.message;
+    }
+    console.log(validationResult);
+    try {
+        rangeValidator.from = 4;
+        validationResult = rangeValidator.validate(5); 
+    } catch (err) {
+        validationResult = err.message;
+    }
+    console.log(validationResult);
+    try {
+        rangeValidator.to = '10';
+        validationResult = rangeValidator.validate(6); 
+    } catch (err) {
+        validationResult = err.message;
+    }
+    console.log(validationResult);
+    try {
+        rangeValidator.to = Infinity;
+        validationResult = rangeValidator.validate(99); 
+    } catch (err) {
+        validationResult = err.message;
+    }
+    console.log(validationResult);
 } catch (err) {
-    validationResult = err.message;
+    console.log(err.message);
 }
-console.log(validationResult);
-try {
-    validationResult = rangeValidator.validate(5); 
-} catch (err) {
-    validationResult = err.message;
-}
-console.log(validationResult);
-try {
-    validationResult = rangeValidator.validate(6); 
-} catch (err) {
-    validationResult = err.message;
-}
-console.log(validationResult);
